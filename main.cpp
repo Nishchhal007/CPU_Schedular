4ca3d5d0ca4d20061dd9c77aaa2ecbae55fb47ec
Nishhchal Pachouri <84848073+Nishchhal007@users.noreply.github.com>
2022-12-20 10:35:53 +0530

    Files Uploaded

---
+++ main.cpp
@@ -0,0 +1,68 @@
+// #include<bits/stdc++. h>
+#include<iostream>
+
+using namespace std;
+
+//Class process - which will assign values to the data members and print them
+
+class process
+{
+    int pid,arr_time,brust_time,completion_time, turn_around_time, waiting_time,response_time; 
+
+    public:
+
+    //Constructor to assign values to these data members
+
+    process()
+    {
+        pid = 0;
+        arr_time = 0;
+        brust_time = 0;
+        completion_time = 0;
+        turn_around_time = 0;
+        waiting_time = 0;
+        response_time = 0;
+        cout<<"Process ID : "<<pid<<endl;
+        cout<<"Arrival Time : "<<arr_time<<endl;
+        cout<<"Brust Time : "<<brust_time<<endl;
+        cout<<"Completion Time : "<<completion_time<<endl;
+        cout<<"Turn Around Time : "<<turn_around_time<<endl;
+        cout<<"Waiting Time : "<<waiting_time<<endl;
+        cout<<"Response Time : "<<response_time<<endl;
+    }
+
+};
+
+//Class process_creator - which will assign a random arrival time and burst time to each process.
+
+class process_creator
+{
+    // int pid,arr_time,brust_time,completion_time, turn_around_time, waiting_time,response_time; 
+
+
+};
+
+// class Schedular - This will implement the scheduling algorithms.
+
+class Schedular
+{
+    // int pid,arr_time,brust_time,completion_time, turn_around_time, waiting_time,response_time; 
+
+
+};
+
+// class Simulator - This class will start the simulation.
+
+class Simulator{
+
+
+};
+
+
+int main()
+{
+    process p;
+
+    
+    return 0;
+}
