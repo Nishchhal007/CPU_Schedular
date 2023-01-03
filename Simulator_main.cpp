4b74ca4b7a012acd0d7a05cda18c535e9d194a0f
Nishchhal <nishchhal901@gmail.com>
2023-01-03 12:51:01 +0530

    FCFS Implemented Successfully

--- Simulator_main.cpp
+++ Simulator_main.cpp
@@ -3,11 +3,12 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include<queue>
 #include <algorithm>
 #include <utility>
 #include <fstream>
 
-#define num_of_process 20
+#define num_of_process 5
 #define MAX_SIZE 999999
 
 using namespace std;
@@ -17,129 +18,77 @@ using namespace std;
 void starter();
 void menu();
 
-// File Handling Function
 
-void initilize_header_to_status_file();
-void initilize_header_to_process_file();
-void write_update_to_status_file(process_details PD, const char status[], int t);
+// // Class Queue and its Functions
 
-// Class Queue and its Functions
-
-class Queue
-{
-    Process **arr;
-    int front;
-    int back;
-    int size;
-
-public:
-    Queue()
-    {
-        arr = new Process *[MAX_SIZE];
-        front = 0;
-        back = -1;
-        size = 0;
-    };
-    void enqueue(Process x)
-    {
-        if (size == MAX_SIZE)
-        {
-            cout << "Error: Queue is full" << endl;
-            return;
-        }
-        back = (back + 1) % MAX_SIZE;
-        *arr[back] = x;
-        size++;
-    }
-    void dequeue()
-    {
-        if (size == 0)
-        {
-            cout << "Error: Queue is empty" << endl;
-            return;
-        }
-        front = (front + 1) % MAX_SIZE;
-        size--;
-    }
-    Process peak()
-    {
-        if (size == 0)
-        {
-            cout << "Error: Queue is empty" << endl;
-            exit(1);
-        }
-        return *arr[front];
-    }
-    Process back_ele()
-    {
-        if (size == 0)
-        {
-            cout << "Error: Queue is empty" << endl;
-            exit(1);
-        }
-        return *arr[back];
-    }
-    int Size() { return size; }
-    bool isEmpty()
-    {
-        if (front == -1 || front > back)
-        {
-            return true;
-        }
-        return false;
-    }
-    friend class Process;
-    friend class Scheduler;
-    friend class Simulator;
-    friend class Schedular_FCFS;
-};
-
-// void Queue ::enqueue(Process x)
+// // class Queue
 // {
-// }
-// void Queue ::dequeue()
-// {
-//     if (size == 0)
+//     Process **arr;
+//     int front;
+//     int back;
+//     int size;
+
+// public:
+//     Queue()
 //     {
-//         cout << "Error: Queue is empty" << endl;
-//         return;
-//     }
-//     front = (front + 1) % MAX_SIZE;
-//     size--;
-// }
-// Process Queue ::peak()
-// {
-
-//     if (size == 0)
+//         arr = new Process *[MAX_SIZE];
+//         front = 0;
+//         back = -1;
+//         size = 0;
+//     };
+//     void enqueue(Process x)
 //     {
-//         cout << "Error: Queue is empty" << endl;
-//         exit(1);
+//         if (size == MAX_SIZE)
+//         {
+//             cout << "Error: Queue is full" << endl;
+//             return;
+//         }
+//         back = (back + 1) % MAX_SIZE;
+//         *arr[back] = x;
+//         size++;
 //     }
-//     return *arr[front];
-// }
-
-// Process Queue ::back_ele()
-// {
-//     if (size == 0)
+//     void dequeue()
 //     {
-//         cout << "Error: Queue is empty" << endl;
-//         exit(1);
+//         if (size == 0)
+//         {
+//             cout << "Error: Queue is empty" << endl;
+//             return;
+//         }
+//         front = (front + 1) % MAX_SIZE;
+//         size--;
 //     }
-//     return *arr[back];
-// }
-// int Queue::Size()
-// {
-//     return size;
-// }
-
-// bool Queue ::isEmpty()
-// {
-//     if (front == -1 || front > back)
+//     Process peak()
 //     {
-//         return true;
+//         if (size == 0)
+//         {
+//             cout << "Error: Queue is empty" << endl;
+//             exit(1);
+//         }
+//         return *arr[front];
 //     }
-//     return false;
-// }
+//     Process back_ele()
+//     {
+//         if (size == 0)
+//         {
+//             cout << "Error: Queue is empty" << endl;
+//             exit(1);
+//         }
+//         return *arr[back];
+//     }
+//     int Size() { return size; }
+//     bool isEmpty()
+//     {
+//         if (front == -1 || front > back)
+//         {
+//             return true;
+//         }
+//         return false;
+//     }
+//     friend class Process;
+//     friend class Scheduler;
+//     friend class Simulator;
+//     friend class Schedular_FCFS;
+// };
 
 // For storing Various Details of a process
 
@@ -153,10 +102,10 @@ class process_details
     int turn_around_time;
     int waiting_time;
     int response_time;
-    int completion_time;
 
 public:
     friend void write_update_to_status_file(process_details PD, const char status[], int t);
+    friend void write_update_to_process_file(process_details PD);
     friend class Process;
     friend class Process_Creator;
     friend class Scheduler;
@@ -168,9 +117,9 @@ public:
 
 class Process
 {
-    process_details processes_data;
 
 public:
+    process_details processes_data;
     // Constructor For assigning Values
 
     Process(process_details pd)
@@ -222,8 +171,9 @@ public:
     // friend class RRScheduler;
     friend class Process_Creator;
     friend class Scheduler;
-    friend class Simulator;
+    friend class process_details;
     friend class Schedular_FCFS;
+    friend class Simulator;
 };
 
 class Process_Creator
@@ -237,10 +187,10 @@ public:
     {
         process_details pd;
 
-        pd.completion_time = 0;
-        pd.turn_around_time = 0;
-        pd.waiting_time = 0;
-        pd.response_time = 0;
+        pd.completion_time = -1;
+        pd.turn_around_time = -1;
+        pd.waiting_time = -1;
+        pd.response_time = -1;
 
         // Allocates Memory for the Array
 
@@ -284,38 +234,25 @@ public:
     friend class Schedular_FCFS;
 };
 
-class Schedular
-{
 
+// it will compare two processes arrival and 
+
+class FCFS_Comparator
+{
 public:
-    void schedule_processes(Process_Creator pc, int simulation_time, int time_quantum, string sAlgorithm)
+    int operator()(const Process &p1, const Process &p2)
     {
-        // string fa = "FCFS";
-        // string sa = "RR";
-        // string ta = "CFS";
-        if (sAlgorithm == "FCFS")
+        if (p1.get_arr_time() == p2.get_arr_time())
         {
-            Schedular_FCFS Fcfs(pc);
-            // for (int i = 1; i <= simulationTime; i++)
-            // {
-            //     cout << "\nIteration is " << i << endl;
-            //     Fcfs.FCFS(PC, i);
-            // }
-            // while (Fcfs.readyQueue.empty() == false)
-            // {
-            //     // Process P = Fcfs.readyQueue.top();
-            //     // writeDataToProcessesFile(P.Data);
-            //     Fcfs.readyQueue.pop();
-            // }
-            return;
+            return p1.get_pid() > p2.get_pid();
         }
+        return p1.get_arr_time() > p2.get_arr_time();
     }
-    friend class Simulator;
 };
 
 class Schedular_FCFS
 {
-    Queue ready_queue;
+    priority_queue<Process, vector<Process>, FCFS_Comparator> ready_queue;
 
 public:
     Schedular_FCFS(Process_Creator &PC)
@@ -328,160 +265,235 @@ public:
         {
             if (pc.processes[i]->processes_data.arr_time == t)
             {
-                ready_queue.enqueue(Process(pc.processes[i]->processes_data));
+                ready_queue.push(Process(pc.processes[i]->processes_data));
                 write_update_to_status_file(pc.processes[i]->processes_data, "Arrived", t);
                 cout << "t and AT Matched" << pc.processes[i]->processes_data.arr_time << endl;
             }
         }
     }
-    // void writeStatusFile(const char status[], int t)
-    // {
-    //     Process P = readyQueue.top();
-    //     writeDataToStatusFile(P.Data, status, t);
-    //     return;
-    // }
-    // void writeProcessesFile(ProcessDetails_t Data)
-    // {
-    //     writeDataToProcessesFile(Data);
-    // }
+    void write_to_status_file(const char status[], int t)
+    {
+        Process P = ready_queue.top();
+        write_update_to_status_file(P.processes_data, status, t);
+        return;
+    }
+    void write_to_processes_file(process_details process_data)
+    {
+        write_update_to_process_file(process_data);
+    }
     void FCFS(Process_Creator &PC, int t)
     {
         checkProcessesArrival(PC, t);
-        if (ready_queue.isEmpty() == false)
+        if (ready_queue.empty() == false)
         {
-            Process P = ready_queue.peak();
+            Process P = ready_queue.top();
             if (P.processes_data.brust_time != 0)
             {
-                            // writeStatusFile("Running", t);
-                //         P.Data.burstTime -= 1;
-                //         if (P.Data.responseTime == -1)
-                //         {
-                //             P.Data.responseTime = t - P.Data.arrivalTime;
-                //         }
-                //         readyQueue.pop();
-                //         readyQueue.push(Process(P.Data));
-                //     }
-                
-                // P.printProcessDetails();
-                // P.printPidATBT();
-                // }
+                write_to_status_file("Running", t);
+                P.processes_data.brust_time -= 1;
+                if (P.processes_data.response_time == -1)
+                {
+                    P.processes_data.response_time = t - P.processes_data.arr_time;
+                }
+                ready_queue.pop();
+                ready_queue.push(Process(P.processes_data));
             }
-            friend class Scheduler;
-        };
-
-        class Simulator
-        {
-        private:
-            int simulation_time;
-            int time_quantum;
-            int num_of_processes;
-            string sAlgorithm;
-
-        public:
-            Simulator(int sT, int tQ, string sA)
+            else
             {
-                simulation_time = sT;
-                time_quantum = tQ;
-                num_of_processes = num_of_process;
-                sAlgorithm = sA;
+                write_to_status_file("Exit", t);
+                P.processes_data.completion_time = t;
+                P.processes_data.turn_around_time = P.processes_data.completion_time - P.processes_data.arr_time;
+                P.processes_data.waiting_time = P.processes_data.turn_around_time - P.processes_data.brust_time;
+                write_to_processes_file(P.processes_data);
+                ready_queue.pop();
+                if (ready_queue.empty() == false)
+                {
+                    P = ready_queue.top();
+                    P.processes_data.brust_time -= 1;
+                    write_to_status_file("Running", t);
+                    if (P.processes_data.response_time == -1)
+                    {
+                        P.processes_data.response_time = t - P.processes_data.arr_time;
+                    }
+                    ready_queue.pop();
+                    ready_queue.push(Process(P.processes_data));
+                }
+                printf("\nProcess Completed...\n");
             }
+            // P.print_process_details();
+            // P.print_pid_at_bt();
+        }
+    }
+    friend class Schedular;
+};
 
-            void init_simulation()
+class Schedular
+{
+
+public:
+    void schedule_processes(Process_Creator pc, int simulation_time, int time_quantum, string sAlgorithm)
+    {
+        // string fa = "FCFS";
+        // string sa = "RR";
+        // string ta = "CFS";
+        if (sAlgorithm == "FCFS")
+        {
+            Schedular_FCFS Fcfs(pc);
+            for (int i = 1; i <= simulation_time; i++)
             {
-                Process_Creator pc;
-                Schedular s;
-                s.schedule_processes(pc, simulation_time, time_quantum, sAlgorithm);
+                cout << "\nIteration is " << i << endl;
+                Fcfs.FCFS(pc, i);
             }
-            friend class Scheduler;
-        };
-        int main()
-        {
-            starter();
-
-            int time_quantum;
-            int simulation_time;
-            cout << "Enter stimulation time (in secs) : ";
-            cin >> simulation_time;
-            string salgorithm;
-            menu();
-            cin >> salgorithm;
-            // For RR
-
-            if (salgorithm.compare("RR") == 0)
+            while (Fcfs.ready_queue.empty() == false)
             {
-                cout << "Enter time Quantum (in ms) : ";
-                cin >> time_quantum;
+                Process P = Fcfs.ready_queue.top();
+                write_update_to_process_file(P.processes_data);
+                Fcfs.ready_queue.pop();
             }
-
-            // Initilizing Headers in status.txt and process.txt
-
-            initilize_header_to_status_file();
-            initilize_header_to_process_file();
-
-            // Start Simulation
-
-            Simulator Simulation(simulation_time, time_quantum, salgorithm);
-            Simulation.init_simulation();
-
-            return 0;
+            return;
         }
+    }
+    friend class Process;
+    friend class Simulator;
+};
 
-        // File handling Functions
 
-        void initilize_header_to_status_file()
-        {
-            FILE *fp;
-            fp = fopen("status.txt", "w");
-            if (fp == NULL)
-            {
-                printf("Error file opening status.txt !");
-                exit(1);
-            }
-            fprintf(fp, "Milli Seconds(ms)\t\t\tProcess Id\t\t\tStatus\n");
-            fclose(fp);
-        }
+class Simulator
+{
+private:
+    int simulation_time;
+    int time_quantum;
+    int num_of_processes;
+    string sAlgorithm;
 
-        void initilize_header_to_process_file()
-        {
-            FILE *fp;
-            fp = fopen("process.txt", "w");
-            if (fp == NULL)
-            {
-                printf("Error file opening process.txt !");
-                exit(1);
-            }
-            fprintf(fp, "Process Id\t\tArrival Time\t\tBurst Time\t\tCompletion Time\t\tTurn Around Time\t\tWaiting Time\t\tResponse Time\n");
-            fclose(fp);
-        }
+public:
+    Simulator(int sT, int tQ, string sA)
+    {
+        simulation_time = sT;
+        time_quantum = tQ;
+        num_of_processes = num_of_process;
+        sAlgorithm = sA;
+    }
 
-        void write_update_to_status_file(process_details PD, const char status[], int t)
-        {
-            FILE *fp;
-            fp = fopen("status.txt", "a");
-            if (fp == NULL)
-            {
-                printf("\nError file Updating... status.txt !\n");
-            }
-            fprintf(fp, "%010d\t\t%010d\t\t%s\n", t, PD.pid, status);
-            fclose(fp);
-        }
+    void init_simulation()
+    {
+        Process_Creator pc;
+        Schedular s;
+        s.schedule_processes(pc, simulation_time, time_quantum, sAlgorithm);
+    }
+    friend class Scheduler;
+};
 
-        void menu()
-        {
-            cout << "\n**********************************************************\n"
-                 << endl;
-            cout << "Enter 'FCFS' for First Come First Serve (FCFS) " << endl;
-            cout << "Enter 'RR' for Round Robin Scheduling " << endl;
-            cout << "Enter 'CFS' for Completely Fair Scheduler (CFS) " << endl;
-            cout << "\n**********************************************************\n"
-                 << endl;
-        }
 
-        void starter()
-        {
-            cout << "\n\n\t***************************************************************\n"
-                 << endl;
-            cout << "\t\t\t Welcome to my CPU Schedular " << endl;
-            cout << "\n\t***************************************************************\n"
-                 << endl;
-        }
+// File Handling Function
+
+void initilize_header_to_status_file();
+void initilize_header_to_process_file();
+void write_update_to_status_file(process_details PD, const char status[], int t);
+
+
+int main()
+{
+    starter();
+
+    int time_quantum;
+    int simulation_time;
+    cout << "Enter stimulation time (in secs) : ";
+    cin >> simulation_time;
+    //Converting stimulation time into mili-Seconds
+    
+    simulation_time = simulation_time*1000;
+    string salgorithm;
+    menu();
+    cin >> salgorithm;
+    // For RR
+
+    if (salgorithm.compare("RR") == 0)
+    {
+        cout << "Enter time Quantum (in ms) : ";
+        cin >> time_quantum;
+    }
+
+    // Initilizing Headers in status.txt and process.txt
+
+    initilize_header_to_status_file();
+    initilize_header_to_process_file();
+
+    // Start Simulation
+
+    Simulator Simulation(simulation_time, time_quantum, salgorithm);
+    Simulation.init_simulation();
+
+    return 0;
+}
+
+// File handling Functions
+
+void initilize_header_to_status_file()
+{
+    FILE *fp;
+    fp = fopen("status.txt", "w");
+    if (fp == NULL)
+    {
+        printf("Error file opening status.txt !");
+        exit(1);
+    }
+    fprintf(fp, "Milli Seconds(ms)\t\t\tProcess Id\t\t\tStatus\n");
+    fclose(fp);
+}
+
+void initilize_header_to_process_file()
+{
+    FILE *fp;
+    fp = fopen("process.txt", "w");
+    if (fp == NULL)
+    {
+        printf("Error file opening process.txt !");
+        exit(1);
+    }
+    fprintf(fp, "Process Id\t\tArrival Time\t\tBurst Time\t\tCompletion Time\t\tTurn Around Time\t\tWaiting Time\t\tResponse Time\n");
+    fclose(fp);
+}
+
+void write_update_to_status_file(process_details PD, const char status[], int t)
+{
+    FILE *fp;
+    fp = fopen("status.txt", "a");
+    if (fp == NULL)
+    {
+        printf("\nError file Updating... status.txt !\n");
+    }
+    fprintf(fp, "%010d\t\t\t%010d\t\t\t%s\n", t, PD.pid, status);
+    fclose(fp);
+}
+void write_update_to_process_file(process_details PD)
+{
+    FILE *fp;
+    fp = fopen("process.txt", "a");
+    if (fp == NULL)
+    {
+        printf("\nError file Updating... process.txt !\n");
+    }
+    fprintf(fp, "%010d\t\t%012d\t\t%010d\t\t%015d\t\t%016d\t\t%012d\t\t%013d\n", PD.pid, PD.arr_time, PD.brust_time, PD.completion_time, PD.turn_around_time, PD.waiting_time, PD.response_time);
+    fclose(fp);
+}
+
+void menu()
+{
+    cout << "\n**********************************************************\n"
+         << endl;
+    cout << "Enter 'FCFS' for First Come First Serve (FCFS) " << endl;
+    cout << "Enter 'RR' for Round Robin Scheduling " << endl;
+    cout << "Enter 'CFS' for Completely Fair Scheduler (CFS) " << endl;
+    cout << "\n**********************************************************\n"
+         << endl;
+}
+
+void starter()
+{
+    cout << "\n\n\t***************************************************************\n"
+         << endl;
+    cout << "\t\t\t Welcome to my CPU Schedular " << endl;
+    cout << "\n\t***************************************************************\n"
+         << endl;
+}
